33d532f6378434c71e91c722eea88e630f30c884
X6STRIVE <X6STRIVE@IN05N001NS>
2024-02-20 23:12:34 +0530

    Add project files.

---
+++ Catalog/Controllers/CatalogController.cs
@@ -0,0 +1,32 @@
+﻿using Catalog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private static readonly List<Product> _products = new List<Product>
+        {
+            new Product(1, "Laptop", "A high performance laptop", 1200.00m),
+            new Product(2, "Smartphone", "An advanced smartphone", 800.00m),
+            new Product(3, "Tablet", "A user-friendly tablet for all your needs", 600.00m),
+        };
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetAllProducts()
+        {
+            return _products;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProductById(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+    }
+}
---
+++ Customer/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Customer.Models;
+
+namespace Customer.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private static readonly CustomerModel _dummyCustomer = new CustomerModel();
+        [HttpGet]
+        public ActionResult<CustomerModel> GetCustomerModel()
+        {
+            return _dummyCustomer;
+        }
+        [HttpPost]
+        public IActionResult BuyProduct()
+        {
+            // Get the product from the Catalog
+            // Make call to the cart using the ID
+        }
+    }
+}
